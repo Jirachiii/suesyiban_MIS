423386d85947b6fcadd8599b86d1299b96261c55
Jin <305648262@qq.com>
2016-03-09 01:24:35 +0800

    课表已从学校更新，故更新课表逻辑恢复正常

--- views/site/index.php
+++ views/site/index.php
@@ -8,16 +8,21 @@ use yii\grid\GridView;
         <meta charset="UTF-8">
         <title>Document</title>
         <link rel="stylesheet" href="css/main/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="css/site.css">
         <script src="js/jquery.min.js"></script>
     </head>
     <body onload="setup()">
-        <h1>homepage</h1>
-        <form action="index.php?r=site/logout" method="post">
-        	<input type="submit" value="退出">
-        </form>
-        <br>
         <a href="index.php?r=user/item"id="hrefuser">用户界面</a>
         <a href="index.php?r=admin/index" id="hrefadmin">管理员界面</a>
+        <div id="effect1" class="">
+        <div id="effect2" class="">
+                <div id="left" class="indeximg1">
+                    
+                </div>
+                <div id="right" class="">
+                    
+                </div>
+        </div>           
         <script>
             if(<?=	$userstatus ?>!=1){
                 $("#hrefadmin").hide()
